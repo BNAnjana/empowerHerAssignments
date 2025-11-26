1f4ad3db26c53b50617a340ad9fe637f988f39fc
BNAnjana <nagaanjana003@gmail.com>
2025-11-26 16:49:02 +0530

    This is CB demo

---
+++ Module_2/Day_15_Async_JS-II/Q11/basic-cb.js
@@ -0,0 +1,14 @@
+// Basic Callback Demonstration
+
+function displayMessage(name) {
+    console.log(`Hello, ${name}`);
+}
+
+function getUserInput(callback) {
+    setTimeout(() => {
+        const username = "Alice";
+        callback(username);
+    }, 1000);
+}
+
+getUserInput(displayMessage);
