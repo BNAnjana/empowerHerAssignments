43120371f9aecffa951d14c6d72786125cc3bc82
BNAnjana <nagaanjana003@gmail.com>
2025-12-17 22:47:01 +0530

    user data

---
+++ Module_3/Day_5/Q1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserData from "./UserData";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <UserData />
+    </div>
+  );
+}
+
+export default App;
