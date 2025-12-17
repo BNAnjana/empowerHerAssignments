118403a86706290efdffcab542d373536cff4a95
BNAnjana <nagaanjana003@gmail.com>
2025-12-17 13:29:10 +0530

    This is counter app

---
+++ Module_3/Day_4/Q1/src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./Counter";
+function App() {
+  return (
+    <div className="counter">
+      <h1>Welcome to the Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ Module_3/Day_4/Q1/src/Counter.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+function Counter() {
+  const [count, setCount] = useState(0);
+    return (
+    <div className="counter">
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    )
+}
+
+export default Counter;
