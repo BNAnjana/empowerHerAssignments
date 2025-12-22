64903a382f57a63f39c7031e4546aa8f208c5221
BNAnjana <nagaanjana003@gmail.com>
2025-12-22 16:38:28 +0530

    props drilling using context api

---
+++ Module_3/Day_8/Q1/src/App.jsx
@@ -0,0 +1,12 @@
+import Component1 from "./Component1";
+
+function App() {
+  return (
+    <>
+      <h2 style={{color: "hotpink"}}>Props Drilling using Context API</h2>
+      <Component1 />
+    </>  
+  );
+}
+
+export default App;
