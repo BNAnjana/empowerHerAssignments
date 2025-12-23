b2663679044de11952ba8731680c1a06a26b4c0f
BNAnjana <nagaanjana003@gmail.com>
2025-12-23 16:42:09 +0530

    routing 2

---
+++ Module_3/Day_10/Q1/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from "./Login";
+import Home from './Home';
+import ProtectedRoute from "./ProtectedRoute";
+import TodoDetails from "./TodoDetails";
+import Todos from "./Todos";
+import "./App.css";
+
+function App(){
+    return(
+        <div>
+            <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path='/todos' element={<ProtectedRoute>
+                    <Todos/>
+                </ProtectedRoute>}/>
+                <Route path='/todos/:todoId' element={<ProtectedRoute>
+                    <TodoDetails/>
+                </ProtectedRoute>}/>
+            </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+export default App;
