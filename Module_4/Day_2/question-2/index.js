ad3392111479e82aa49a9796b59deb50090450d0
BNAnjana <nagaanjana003@gmail.com>
2026-01-12 18:14:57 +0530

    Custom modules

---
+++ Module_4/Day_2/question-2/index.js
@@ -0,0 +1,10 @@
+
+import { checkPrime } from "./math.js";
+
+console.log(checkPrime(2));   // true
+console.log(checkPrime(3));   // true
+console.log(checkPrime(5));   // true
+console.log(checkPrime(4));   // false
+console.log(checkPrime(17));  // true
+console.log(checkPrime(20));  // false
+console.log(checkPrime(65));  // false
