471707aff74ed59456dded89d10c910bfd57d112
BNAnjana <nagaanjana003@gmail.com>
2026-01-20 23:40:49 +0530

    express multer

---
+++ Module_4/Day_7/Q2/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import usersRouter from "./routes/users.routes.js";
+
+const app = express();
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+
+app.use("/users", usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ Module_4/Day_7/Q2/src/middleware/upload.middleware.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (!file.mimetype.startsWith("image/")) {
+    return cb(new Error("Only image files are allowed"), false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024 // 2MB
+  },
+  fileFilter
+});
+
+export default upload;
